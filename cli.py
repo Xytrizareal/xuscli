391db6c140b7312d3780bd9a67d165aceca5a4d9
Xytrizareal <158840904+Xytrizareal@users.noreply.github.com>
2024-03-02 20:19:55 -0700

    new!

--- cli.py
+++ cli.py
@@ -8,6 +8,7 @@ try:
 except ImportError:
     os.system(sys.executable + " -m pip install requests")
     import requests
+import datetime
 
 def save_api_key(api_key):
     home = str(Path.home())
@@ -60,6 +61,8 @@ if __name__ == "__main__":
     parser.add_argument('--config', type=str, help='Use this option to save your API key. Example: --config apikey=your_api_key_here')
     parser.add_argument('--upload', type=str, help='Use this option to upload a file to the server. Example: --upload filepath')
     parser.add_argument('--delete', type=str, help='Use this option to delete a file from the server. Example: --delete deletion_key')
+    parser.add_argument('--fileinfo', type=str, help='Use this option to get information about a file. Example: --fileinfo fileid')
+    parser.add_argument('--userinfo', type=str, help='Use this option to get information about a user. Example: --userinfo uid')
 
     if len(sys.argv)==1:
         parser.print_help(sys.stderr)
@@ -107,3 +110,48 @@ if __name__ == "__main__":
             print("File deleted successfully")
         else:
             print("Error: " + data["response"] + " - " + str(response.status_code))
+
+    if args.fileinfo:
+        api_key = get_config("api_key")
+        if api_key is not None:
+            fileid = args.fileinfo
+            response = requests.get("https://upload.xytriza.com/api/getFileInfo.php", headers={"key": api_key, "fileid": fileid})
+            data = response.json()
+
+            if data["success"] == True and response.status_code == 200:
+                fileinfo = data['fileinfo']
+                uploaded_at = datetime.datetime.fromtimestamp(fileinfo["uploadedAt"], datetime.timezone.utc)
+                uploaded_at_str = uploaded_at.strftime("%Y-%m-%d %H:%M:%S")
+                print("File Info:")
+                print("ID: " + fileinfo["id"])
+                print("Uploader UID: " + str(fileinfo["uploaderUid"]))
+                print("Size: " + fileinfo["sizeFormatted"])
+                print("Uploaded At: " + uploaded_at_str + " (UTC)")
+                print("Filename: " + fileinfo["fileName"])
+            else:
+                print("Error: " + data["response"] + " - " + str(response.status_code))
+        else:
+            print("API key not found. Please save your API key using --config apikey=your_api_key_here")
+
+    if args.userinfo:
+        api_key = get_config("api_key")
+        if api_key is not None:
+            uid = args.userinfo
+            response = requests.get("https://upload.xytriza.com/api/getUserInfo.php", headers={"key": api_key, "uid": uid})
+            data = response.json()
+
+            if data["success"] == True and response.status_code == 200:
+                userinfo = data['userinfo']
+                registered_at = datetime.datetime.fromtimestamp(userinfo["registerTime"], datetime.timezone.utc)
+                registered_at_str = registered_at.strftime("%Y-%m-%d %H:%M:%S")
+                print("User ID: " + str(userinfo['uid']))
+                print("Username: " + userinfo['username'])
+                print("Display Name: " + userinfo['displayName'])
+                print("Discord ID: " + userinfo['discordId'])
+                print("Role: " + userinfo['role'])
+                print("Register Time: " + registered_at_str + " (UTC)")
+                print("Avatar URL: " + userinfo['avatar'])
+            else:
+                print("Error: " + data["response"] + " - " + str(response.status_code))
+        else:
+            print("API key not found. Please save your API key using --config apikey=your_api_key_here")
